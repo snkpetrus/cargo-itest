fa369d49d76f25af93efb24e13a09607f20ac95a
salomo.petrus <salomo.petrus@0bb5c09c-de92-11dd-bd7b-b12b6590754f>
2009-01-30 13:18:59 +0000

    Adde open source licence.

--- trunk/src/test/java/nl/tranquilizedquality/itest/TestAppTomcatDeploymentTest.java
+++ trunk/src/test/java/nl/tranquilizedquality/itest/TestAppTomcatDeploymentTest.java
@@ -1,14 +1,17 @@
-/**
- * Project: cargo-itest Created on: 30 jan 2009 File:
- * TestAppTomcatDeploymentTest.java Package: nl.tranquilizedquality.itest
+/*
+ * Copyright 2009 Salomo Petrus
  * 
- * Copyright (c) 2009 Tranquilized Quality www.tq-quality.nl All rights
- * reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
  * 
- * This software is the confidential and proprietary information of Tranquilized
- * Quality ("Confidential Information"). You shall not disclose such
- * Confidential Information and shall use it only in accordance with the terms
- * of the license agreement you entered into with Tranquilized Quality.
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
  */
 package nl.tranquilizedquality.itest;
 
