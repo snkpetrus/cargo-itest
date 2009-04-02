1349781544f782b9ab990c116038d364156b6879
salomo.petrus <salomo.petrus@0bb5c09c-de92-11dd-bd7b-b12b6590754f>
2009-04-02 10:46:55 +0000

    [maven-scm] copy for tag cargo-itest-1.1.1

---
+++ tags/cargo-itest-1.1.1/src/main/java/nl/tranquilizedquality/itest/cargo/exception/DeployException.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2009 Salomo Petrus
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package nl.tranquilizedquality.itest.cargo.exception;
+
+/**
+ * Use this exception when something goes wrong during deploying of your
+ * application.
+ * 
+ * @author Salomo Petrus (sape)
+ * @since 1 apr 2009
+ * 
+ */
+public class DeployException extends RuntimeException {
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 6530739596346697399L;
+
+    /**
+     * Default constructor.
+     */
+    public DeployException() {
+    }
+
+    /**
+     * Constructor where you can specify the error message.
+     * 
+     * @param msg The error message that will be used.
+     */
+    public DeployException(String msg) {
+        super(msg);
+    }
+
+    /**
+     * Constructor where you can specify the error message and the cause of this
+     * exception.
+     * 
+     * @param msg The error message that will be used.
+     * @param cause The {@link Throwable} that caused this exception.
+     */
+    public DeployException(String msg, Throwable cause) {
+        super(msg, cause);
+    }
+}
