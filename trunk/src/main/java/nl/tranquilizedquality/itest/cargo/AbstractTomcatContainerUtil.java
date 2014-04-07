401211323f3927bcf1b353a65c81b00c4a8993b4
salomo.petrus <salomo.petrus@0bb5c09c-de92-11dd-bd7b-b12b6590754f>
2014-04-07 18:49:15 +0000

    Changed JPA port and moved system properties settings to correct location.

--- trunk/src/main/java/nl/tranquilizedquality/itest/cargo/AbstractTomcatContainerUtil.java
+++ trunk/src/main/java/nl/tranquilizedquality/itest/cargo/AbstractTomcatContainerUtil.java
@@ -108,6 +108,9 @@ public abstract class AbstractTomcatContainerUtil extends AbstractInstalledConta
          */
         super.setupContainer();
 
+        systemProperties.put(TomcatPropertySet.AJP_PORT, ajpPort.toString());
+        systemProperties.put(GeneralPropertySet.RMI_PORT, rmiPort.toString());
+
         setupConfiguration();
     }
 
@@ -144,9 +147,6 @@ public abstract class AbstractTomcatContainerUtil extends AbstractInstalledConta
         configuration.setProperty(ServletPropertySet.PORT, containerPort.toString());
         configuration.setProperty(TomcatPropertySet.AJP_PORT, ajpPort.toString());
         configuration.setProperty(GeneralPropertySet.RMI_PORT, rmiPort.toString());
-
-        systemProperties.put(TomcatPropertySet.AJP_PORT, ajpPort.toString());
-        systemProperties.put(GeneralPropertySet.RMI_PORT, rmiPort.toString());
 
         /*
          * Iterate over all available deployable locations.
