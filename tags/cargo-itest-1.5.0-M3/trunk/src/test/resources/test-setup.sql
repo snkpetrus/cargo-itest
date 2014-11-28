2d707c77857cec67f4c927bb1db9353c47a40bda
salomo.petrus@gmail.com <salomo.petrus@gmail.com@0bb5c09c-de92-11dd-bd7b-b12b6590754f>
2014-11-28 14:17:57 +0000

    Invalid tag

--- tags/cargo-itest-1.5.0-M3/trunk/src/test/resources/test-setup.sql
+++
@@ -1,8 +0,0 @@
-    drop table if exists ABSTRACT_ACCOUNT;
-
-    create table ABSTRACT_ACCOUNT (
-        id bigint not null,
-        primary key (id)
-    );
-    
-    insert into ABSTRACT_ACCOUNT VALUES (1);
